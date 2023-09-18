ae593081d81839342d398deb76915c75833ed561
stuardo asus-tuf <stuardo.lucho@pucp.edu.pe>
2023-09-18 17:42:26 +0100

    proyecto con validaciones

--- src/main/java/com/example/clase6gtics/controller/ProductController.java
+++ src/main/java/com/example/clase6gtics/controller/ProductController.java
@@ -4,12 +4,17 @@ import com.example.clase6gtics.entity.Product;
 import com.example.clase6gtics.repository.CategoryRepository;
 import com.example.clase6gtics.repository.ProductRepository;
 import com.example.clase6gtics.repository.SupplierRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.beans.PropertyEditorSupport;
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Controller
@@ -40,20 +45,29 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public String guardarProducto(@ModelAttribute("product") Product product, RedirectAttributes attr, Model model) {
-        if (product.getProductname().equals("gaseosa")) {
-            model.addAttribute("msg","Error al crear producto");
+    public String guardarProducto(RedirectAttributes attr, Model model,
+                                  @ModelAttribute("product") @Valid Product product, BindingResult bindingResult) {
+        if (!bindingResult.hasErrors()) { //si no hay errores, se realiza el flujo normal
+
+            if (product.getProductname().equals("gaseosa")) {
+                model.addAttribute("msg", "Error al crear producto");
+                model.addAttribute("listaCategorias", categoryRepository.findAll());
+                model.addAttribute("listaProveedores", supplierRepository.findAll());
+                return "product/editFrm";
+            } else {
+                if (product.getId() == 0) {
+                    attr.addFlashAttribute("msg", "Producto creado exitosamente");
+                } else {
+                    attr.addFlashAttribute("msg", "Producto actualizado exitosamente");
+                }
+                productRepository.save(product);
+                return "redirect:/product";
+            }
+
+        } else { //hay al menos 1 error
             model.addAttribute("listaCategorias", categoryRepository.findAll());
             model.addAttribute("listaProveedores", supplierRepository.findAll());
             return "product/editFrm";
-        } else {
-            if (product.getId() == 0) {
-                attr.addFlashAttribute("msg", "Producto creado exitosamente");
-            } else {
-                attr.addFlashAttribute("msg", "Producto actualizado exitosamente");
-            }
-            productRepository.save(product);
-            return "redirect:/product";
         }
     }
 
@@ -75,8 +89,7 @@ public class ProductController {
     }
 
     @GetMapping("/delete")
-    public String borrarTransportista(Model model,
-                                      @RequestParam("id") int id,
+    public String borrarTransportista(@RequestParam("id") int id,
                                       RedirectAttributes attr) {
 
         Optional<Product> optProduct = productRepository.findById(id);
@@ -88,5 +101,22 @@ public class ProductController {
         return "redirect:/product";
 
     }
+
+   /* @InitBinder("product")
+    public void validador(WebDataBinder binder) {
+
+        binder.registerCustomEditor(BigDecimal.class, "unitprice", new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                try{
+                    BigDecimal bigDecimal = new BigDecimal(text);
+                    this.setValue(bigDecimal);
+                }catch (NumberFormatException e){
+                    this.setValue(0);
+                }
+            }
+        });
+    }*/
+
 
 }
--- src/main/java/com/example/clase6gtics/entity/Product.java
+++ src/main/java/com/example/clase6gtics/entity/Product.java
@@ -1,5 +1,6 @@
 package com.example.clase6gtics.entity;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,8 +17,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "productid")
     private int id;
+
     @Column(nullable = false)
+    @Size(max = 40,message = "Solo se soportan 40 caractéres")
+    @NotBlank
     private String productname;
+
     @ManyToOne
     @JoinColumn(name = "supplierid")
     private Supplier supplier;
@@ -25,9 +30,21 @@ public class Product {
     @JoinColumn(name = "categoryid")
     private Category category;
     private String quantityperunit;
+    
+    @Digits(integer = 10, fraction = 4)
+    @Positive
     private BigDecimal unitprice;
+
+    @Digits(integer = 5,fraction = 0)
+    @Min(value = 0)
+    @Max(value = 32767,message = "el valor máximo es 32767")
     private int unitsinstock;
+
+    @Digits(integer = 5,fraction = 0)
+    @Min(value = 0)
+    @Max(value = 32767)
     private int unitsonorder;
+
     private int reorderlevel;
     @Column(nullable = false)
     private boolean discontinued;
